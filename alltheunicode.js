2a10ba99281d09ec1b13ab92ca6a7d3012ffc41d
Blue Pigeons <onlysky@hotmail.co.uk>
2017-02-26 18:10:47 +0000

    finally removing alerts because basically I am being an idiot again and nothing was wrong with alltheunicode at all I had fixed everything correctly phew.

--- alltheunicode.js
+++ alltheunicode.js
@@ -31383,7 +31383,6 @@ var atu_setup_IME_area1 = function(thisArea) {
   var theCurrentIME = thisArea.data( 'ime' );
   theCurrentIME.enable();
   if (!isUseless($langSelector)) {
-  	alert("starting the second part now");
     atu_setup_IME_area2(thisArea, theCurrentIME);
   };
 };
@@ -31420,7 +31419,6 @@ var atu_load_scripts = function(the_src, callback_func, callback_var) {
 var atu_have_all_ime_scripts_loaded = false;
 
 var atu_all_the_ime_scripts_have_now_loaded = function(callback_func) {
-	alert("all the scripts have loaded now");
 	atu_have_all_ime_scripts_loaded = true;
 	callback_func();
 };
@@ -31447,8 +31445,6 @@ var atu_all_the_scripts = function(callback_func) {
 };
 
 var setupIMElisteners = function(by_button, initialise_options, scripted_btn) {
-
-	alert("setting up event listeners now");
 
 	if ( by_button ) {
 	  $(".polyanno-enable-IME").css("display", "none");
